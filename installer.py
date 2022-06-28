3f463b9005a4d07038c3d2530ddd9444a5ba4056
scardenasb <84429399+scardenasb@users.noreply.github.com>
2022-06-28 03:16:08 -0400

    update

--- installer.py
+++ installer.py
@@ -2,14 +2,18 @@ import os
 
 
 # Create the file repository configuration:
-os.system('sudo sh -c - echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list')
+os.system(
+    'sudo sh -c - echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+)
 
 # Import the repository signing key:
-os.system('wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -')
+os.system(
+    "wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -"
+)
 
 # Update the package lists:
-os.system('sudo apt-get update')
+os.system("sudo apt-get update")
 
 # Install the latest version of PostgreSQL.
 # If you want a specific version, use 'postgresql-12' or similar instead of 'postgresql':
-os.system('sudo apt-get -y install postgresql')
+os.system("sudo apt-get -y install postgresql")
